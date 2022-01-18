ff4e45e2229ea8d5bdcf1607fb4b3cee010314d2
Albertov19 <albertoviera@gmail.com>
2022-01-18 19:30:23 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# xcmTools
+
+Different set of tools with the Polkadot API.
+
+**Use at your own risk!**
+
+## Getting Started
+
+Install packages:
+
+```
+yarn
+```
+
+Then, run script with `ts-node` depending on the file
+
+
+## Scripts
+
+`fetchAssets` gets a list of XC-20 assets in the given network. Returns the name, symbol, decimals and token's precompile address
---
+++ fetchAssets.ts
@@ -0,0 +1,55 @@
+/*
+  Simple code snippet to fetch the list of assets created and calculated
+  their corresponding XC-20 address
+
+  // XC-20 Interface: https://github.com/PureStake/moonbeam/blob/master/precompiles/assets-erc20/ERC20.sol
+*/
+import { ApiPromise, WsProvider } from '@polkadot/api';
+import { bnToHex, stringToHex } from '@polkadot/util';
+
+// Variables
+let assetsList = Array();
+let assetMetadata;
+let assetID = BigInt(0);
+let hexID;
+
+// Create Provider
+//Moonriver
+//const wsProvider = new WsProvider('wss://wss.moonriver.moonbeam.network');
+//Moonbase
+const wsProvider = new WsProvider('wss://wss.api.moonbase.moonbeam.network');
+
+const main = async () => {
+  // Wait for Provider
+  const api = await ApiPromise.create({
+    provider: wsProvider,
+  });
+  await api.isReady;
+
+  // Get all the XC-20 Assets
+  const assets = await api.query.assets.asset.keys();
+
+  for (let i in assets) {
+    let assetTemp = assets[i];
+
+    // Get the Asset ID
+    assetID = BigInt(assetTemp.toHuman()[0].replace(/,/g, ''));
+
+    // Transform it to Hex and calculate precompile address by appending FFs
+    let tempHex = bnToHex(assetID);
+    hexID = '0xffffffff' + tempHex.slice(2, tempHex.length);
+
+    // Get XC-20 Asset Metadata
+    assetMetadata = (await api.query.assets.metadata(assetID.toString())).toHuman();
+
+    // Build result (remove isFrozen and deposit)
+    assetMetadata['address'] = hexID;
+    delete assetMetadata['isFrozen'];
+    delete assetMetadata['deposit'];
+    assetsList.push(assetMetadata);
+  }
+
+  console.log(assetsList);
+};
+
+main();
