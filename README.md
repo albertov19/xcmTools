9c4458f7de661080b7a7a7e752efd717e78ae307
Albertov19 <albertoviera@gmail.com>
2022-11-18 15:48:24 +0100

    minor changes

--- README.md
+++ README.md
@@ -17,8 +17,6 @@ Then, run script with `ts-node` depending on the file
 
 ## Scripts
 
-`calculateBatchUnitsPerSecond --n <"network">` calcualtes the encoded call data for a democracy proposal to update the `UnitsPerSecond` for all assets in `assets.json`
-
 `calculateLocalAssetInfo --w <"ws_endpoint"> --a <"asset">` calculates the storage key element, XC-20 address and Asset ID of a given asset. Need to provide the ws provider (`--w`) and the local asset counter (`--a`)  
 
 `calculateMultilocationDerivative --w <"ws_endpoint"> --a <"address"> --p <"para-id">` calculates the multilocaiton-derived address for remote calls through XCM. Need to provide the ws provider (`--w`) of the target parachain, the address (`--a`) of the origin account, and the parachain id (`--p`) of the origin parachain 
@@ -27,7 +25,7 @@ Then, run script with `ts-node` depending on the file
 
 `calculateSovereignAddress --r <"polkadot/kusama/moonbase"> --paraid <"para-id">` prints the sovereign account of a given parachain Id for the relay chain, and for other parachains. Need to provide the relay to use (`--r` either polkadot, kusama or moonbase) and the parachain id
 
-`calculateUnitsPerSeconds --a <"asset name kusama/karura/kintsugi"> --d <"decimals"> --xoc <"WeighUnitsPerXCMInstruction"> --price <"asset-price">` simple tool to calculate the units per second to be charged for a given target price (0.02$ per XCM transfer). It uses the CoinGecko API to fetch the token price, although it can be overwritten with `--price` flag. You need to provide the asset name as supported by CoinGecko (flag `--aset` or `--a`), the decimals (flag --decimals or `-d`) and the cost of each XCM instruction (`--xcm-op-cost` or `--xoc`)
+`calculateUnitsPerSeconds --a <"asset name kusama/karura/kintsugi"> --d <"decimals"> --xoc <"WeighUnitsPerXCMInstruction"> --price <"asset-price">` simple tool to calculate the units per second to be charged for a given target price (0.02$ per XCM transfer). It uses the CoinGecko API to fetch the token price, although it can be overwritten with `--price` flag. You need to provide the asset name as supported by CoinGecko (flag `--aset` or `--a`), the decimals (flag --decimals or `-d`), there are other inputs that are optional like the cost of each XCM instruction (`--xcm-op-cost` or `--xoc`), by default set to 200M weight units per instruction (Moonbeam & Moonriver)
 
 `calculateBatchUnitsPerSeconds --n <"network moonbeam/moonriver">` script to calculate the batched encoded call data to set the units per second for XCM msgs for all supported types. It wil read the asset information from the `assets.json` file that needs to be manually updated. It uses the CoinGecko API to fetch the token price, although it can be overwritten with a price key and value in the `.json` file. Check `calculateUnitsPerSeconds` for more help.
 
--- calculateUnitsPerSeconds.ts
+++ calculateUnitsPerSeconds.ts
@@ -2,63 +2,64 @@ import yargs from 'yargs';
 import CoinGecko from 'coingecko-api';
 
 const args = yargs.options({
-    decimals: { type: 'string', demandOption: true, alias: 'd' },
-    'xcm-op-cost': { type: 'string', demandOption: false, alias: 'xoc' },
-    price: { type: 'string', demandOption: false, alias: 'p' }, // overwrite price
-    asset: { type: 'string', demandOption: false, alias: 'a' },
+  decimals: { type: 'string', demandOption: true, alias: 'd' },
+  'xcm-op-cost': { type: 'string', demandOption: false, alias: 'xoc' },
+  price: { type: 'string', demandOption: false, alias: 'p' }, // overwrite price
+  asset: { type: 'string', demandOption: false, alias: 'a' },
 }).argv;
 
 async function main() {
-    // Target Price in USD
-    const targetPrice = BigInt(10 ** args['decimals'] * 0.02); // 2 CENTS USD
+  // Target Price in USD
+  const targetPrice = BigInt(10 ** args['decimals'] * 0.02); // 2 CENTS USD
 
-    const decimalsFactor = 10 ** args['decimals'];
+  const decimalsFactor = 10 ** args['decimals'];
 
-    // Start CoinGecko API Client
-    const CoinGeckoClient = new CoinGecko();
-    let tokenPrice;
-    let tokenData = {} as any;
+  // Start CoinGecko API Client
+  const CoinGeckoClient = new CoinGecko();
+  let tokenPrice;
+  let tokenData = {} as any;
 
-    // Get XCM Execution Total Cost
-    let xcmTotalCost = BigInt(4 * 200000000);
-    if (args['xcm-op-cost']) {
-        xcmTotalCost = BigInt(4 * args['xcm-op-cost']);
-    }
+  // Get XCM Execution Total Cost
+  let xcmTotalCost = BigInt(4 * 200000000);
+  if (args['xcm-op-cost']) {
+    xcmTotalCost = BigInt(4 * args['xcm-op-cost']);
+  }
 
-    // Get Token Price - If not provided it will use CoinGecko API to get it
-    if (!args['price']) {
-        if (args['asset']) {
-            tokenData = await CoinGeckoClient.simple.price({
-                ids: args['asset'],
-                vs_currencies: 'usd',
-            });
-        } else {
-            console.error(
-                'You need to provide either an asset name with <--a> or a fixed price with <--p>'
-            );
-        }
-
-        tokenPrice = BigInt(Math.round(decimalsFactor * tokenData.data[args['asset']].usd));
+  // Get Token Price - If not provided it will use CoinGecko API to get it
+  if (!args['price']) {
+    if (args['asset']) {
+      tokenData = await CoinGeckoClient.simple.price({
+        ids: args['asset'],
+        vs_currencies: 'usd',
+      });
     } else {
-        // Use given price
-        tokenPrice = BigInt(decimalsFactor * args['price']);
-        tokenData.success = true;
+      console.error('You need to provide either an asset name with <--a> or a fixed price with <--p>');
     }
 
     if (tokenData.success) {
-        //Calculate Units Per Second
-        const unitsPerSecond =
-            (targetPrice * BigInt(10 ** 12) * BigInt(decimalsFactor)) / (xcmTotalCost * tokenPrice);
-
-        console.log(`Token Price is $${tokenPrice.toString() / decimalsFactor}`);
-        console.log(`The UnitsPerSecond need to be set ${unitsPerSecond.toString()}`);
-
-        return unitsPerSecond;
+      tokenPrice = BigInt(Math.round(decimalsFactor * tokenData.data[args['asset']].usd));
     } else {
-        console.error('Token name not supported, note that is token name and not ticker!');
+      console.error(tokenData.data);
     }
+  } else {
+    // Use given price
+    tokenPrice = BigInt(decimalsFactor * args['price']);
+    tokenData.success = true;
+  }
+
+  if (tokenData.success) {
+    //Calculate Units Per Second
+    const unitsPerSecond = (targetPrice * BigInt(10 ** 12) * BigInt(decimalsFactor)) / (xcmTotalCost * tokenPrice);
+
+    console.log(`Token Price is $${tokenPrice.toString() / decimalsFactor}`);
+    console.log(`The UnitsPerSecond need to be set ${unitsPerSecond.toString()}`);
+
+    return unitsPerSecond;
+  } else {
+    console.error('Token name not supported, note that is token name and not ticker!');
+  }
 }
 
 main()
-    .catch(console.error)
-    .finally(() => process.exit());
+  .catch(console.error)
+  .finally(() => process.exit());
